4b8412ef6ea5dd0b28418568dc73d2d4d38b2dc4
pavel-pyl <4469540@gmail.com>
2022-02-14 15:09:08 +0200

    Correct indents

--- src/ArrayUtil.java
+++ src/ArrayUtil.java
@@ -6,23 +6,22 @@ import java.util.List;
  */
 public class ArrayUtil {
 
-    /**
-     * Finds the common elements between two arrays (String values).
-     *
-     * @param array1 first array
-     * @param array2 second array
-     * @return String array with common elements
-     */
-    public static String[] findCommon(String[] array1, String[] array2) {
-        List<String> res = new ArrayList();
-        for (String s1: array1) {
-            for (String s2 : array2) {
-                if (s1.equals(s2) && !res.contains(s2)) {
-                    res.add(s1);
-                }
-            }
+  /**
+   * Finds the common elements between two arrays (String values).
+   *
+   * @param array1 first array
+   * @param array2 second array
+   * @return String array with common elements
+   */
+  public static String[] findCommon(String[] array1, String[] array2) {
+    List<String> res = new ArrayList();
+    for (String s1 : array1) {
+      for (String s2 : array2) {
+        if (s1.equals(s2) && !res.contains(s2)) {
+          res.add(s1);
         }
-
-        return  res.toArray(new String[0]);
+      }
     }
+    return res.toArray(new String[0]);
+  }
 }
--- src/ArraysTest.java
+++ src/ArraysTest.java
@@ -5,47 +5,45 @@ import static org.testng.Assert.assertEquals;
 
 public class ArraysTest {
 
-    /**
-     * Test data for common array elements.
-     *
-     * @return test data array
-     */
-    @DataProvider(name = "ArrayUtil")
-    public static Object[][] arrays() {
-        return new Object[][]{
-                {
-                        new String[]{"Python", "JAVA", "PHP", "C#", "C++", "SQL"},
-                        new String[]{"MySQL", "SQL", "SQLite", "Oracle", "PostgreSQL", "DB2", "JAVA"},
-                        new String[]{"JAVA", "SQL"}
-                },
-                {
-                        new String[]{"here", "is", "some", "text", ".", "text"},
-                        new String[]{"some", "text", "above", "."},
-                        new String[]{"some", "text", "."}
-                },
-                {
-                        new String[]{"Another", "small", "small", "string", ""},
-                        new String[]{"string", "not", "small", "as", "previous", "one", "", "small"},
-                        new String[]{"small", "string", ""}
-                },
-                {
-                        new String[]{"Android", "", "small", "string"},
-                        new String[]{},
-                        new String[]{}
-                },
-                {
-                        new String[]{"Android", "", "small", "string"},
-                        new String[]{"There", "is", "no", "repetitions"},
-                        new String[]{}
-                }
+  /**
+   * Test data for common array elements.
+   *
+   * @return test data array
+   */
+  @DataProvider(name = "ArrayUtil")
+  public static Object[][] arrays() {
+    return new Object[][]{
+        {
+            new String[]{"Python", "JAVA", "PHP", "C#", "C++", "SQL"},
+            new String[]{"MySQL", "SQL", "SQLite", "Oracle", "PostgreSQL", "DB2", "JAVA"},
+            new String[]{"JAVA", "SQL"}
+        },
+        {
+            new String[]{"here", "is", "some", "text", ".", "text"},
+            new String[]{"some", "text", "above", "."},
+            new String[]{"some", "text", "."}
+        },
+        {
+            new String[]{"Another", "small", "small", "string", ""},
+            new String[]{"string", "not", "small", "as", "previous", "one", "", "small"},
+            new String[]{"small", "string", ""}
+        },
+        {
+            new String[]{"Android", "", "small", "string"},
+            new String[]{},
+            new String[]{}
+        },
+        {
+            new String[]{"Android", "", "small", "string"},
+            new String[]{"There", "is", "no", "repetitions"},
+            new String[]{}
+        }
+    };
+  }
 
-        };
-    }
-
-    @Test(dataProvider = "ArrayUtil")
-    public void testFindCommon(String[] array1, String[] array2, String[] expectedResult) {
-
-        assertEquals(ArrayUtil.findCommon(array1, array2), expectedResult,
-                "Common elements are not correct");
-    }
+  @Test(dataProvider = "ArrayUtil")
+  public void testFindCommon(String[] array1, String[] array2, String[] expectedResult) {
+    assertEquals(ArrayUtil.findCommon(array1, array2), expectedResult,
+        "Common elements are not correct");
+  }
 }
